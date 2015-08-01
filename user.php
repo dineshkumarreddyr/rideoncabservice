50bcf164b7874b3d81ac6eb5c91d4aecd8520917
udaya kumar swamy vasa <udayakumarswamy@gmail.com>
2015-08-01 15:01:37 +0530

    user reg baseurl defined

--- user.php
+++ user.php
@@ -50,6 +50,11 @@ function user_signUp($signUpData) {
 			$signUpData->id = $db->lastInsertId();
 			$db = null;
 			$signUpData_id= $signUpData->id;
+
+			// Get request object
+			$req = $app->request;
+			//Get base URI
+			$baseUrl = $req->getUrl()."/";
 			$confirmation_link = $baseUrl . 'user/signup/confirmation?e=' . urlencode($signUpData->email) . '&h=' . $hash ;
 
 			// sending user registrattioin confirmation mail to user
